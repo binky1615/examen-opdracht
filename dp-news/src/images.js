412326a8b68cfcab9b4459b89e8c557efcaf4ab9
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-07-02 11:45:56 +0200

    Games Page

---
+++ dp-news/src/images.js
@@ -0,0 +1,31 @@
+import playstation from './assets/playstation.png';
+import xbox from './assets/xbox.png';
+import switch12 from './assets/switch.png';
+import pc from './assets/pc.png';
+
+
+const images = [
+  {
+    id: 1,
+    title: "Playstation",
+    image: playstation
+  },
+  {
+    id: 2,
+    title: "Xbox",
+    image: xbox
+  },
+    {
+    id: 3,
+    title: "switch",
+    image: switch12
+  },
+  {
+    id: 4,
+    title: "pc",
+    image: pc
+  },
+];
+
+export default images;
+
--- dp-news/src/pages/Games.jsx
+++ dp-news/src/pages/Games.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import images from '../images.js';
 
 function Games() {
+  const gameIDs = [1888930, //The Last of Us Part 1
+                   1623730, //Palworld
+                   2201320, //Date Everything
+                   1091500, //Cyberpunk 2077
+                   1903340, //Claire Obscur
+                   1086940, //Bladurs gate 3
+                   1245620, //Elden Ring
+                   2420110, //Horizon Forbidden West
+                   1172710, //Dune Awakening
+                   1177980, //Little Kitty, Big City
+                   1332010, //Stray
+                   2660460, //Aviassembly
+                   1954200, //Kena Bridge of Spirits
+                   1307580, //TOEM
+                   2933620, //COD BO6
+                   2420660, //NEVA
+                  ];
+
   return (
-    <div>
-      <h1>games</h1>
-    </div>
+    <section className="flex flex-col gap-4">
+      <div className="flex justify-center">
+        <img className="w-full max-w-sm" src={images[0].image} alt={images[0].title} />
+        <img className="w-full max-w-sm" src={images[1].image} alt={images[1].title} />
+        <img className="w-full max-w-sm" src={images[2].image} alt={images[2].title} />
+        <img className="w-full max-w-sm" src={images[3].image} alt={images[3].title} />
+      </div>
+
+      <div className="flex flex-row gap-6 flex-wrap mb-12">
+        {gameIDs.map(id => (
+          <img
+            key={id}
+            src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${id}/header.jpg`}
+            alt={`Cover for game ${id}`}
+            className="w-full max-w-[460px] h-[215px] object-cover rounded-md shadow-md"
+            loading="lazy"
+          />
+        ))}
+      </div>
+    </section>
   );
 }
 
