bdb0f216876c569e61e4bd9f1aab348a5d718d39
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-07-03 13:00:21 +0200

    Update

--- dp-news/src/pages/News.jsx
+++ dp-news/src/pages/News.jsx
@@ -5,10 +5,12 @@ function News() {
   const gameIDs = [3159330, 1623730, 2201320];
 
   return (
+    <section className="">
     <div>
       <h1>Steam Games News</h1>
       <SteamAPI gameIDs={gameIDs} />
     </div>
+    </section>
   );
 }
 
